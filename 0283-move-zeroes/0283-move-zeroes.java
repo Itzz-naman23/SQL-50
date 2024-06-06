a4cca1eb1b07ce9e407be5c160f71287f641e491
Naman-rawat <113328258+Itzz-naman23@users.noreply.github.com>
2024-06-06 22:28:06 +0530

    Time: 2 ms (84.17%), Space: 46.1 MB (41.59%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,23 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         
-        for(int i =0;i<nums.length-1;i++){
-            for(int j=0;j<nums.length-1;j++){
-                if(nums[j]==0){
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
-                }
+        // for(int i =0;i<nums.length-1;i++){
+        //     for(int j=0;j<nums.length-1;j++){
+        //         if(nums[j]==0){
+        //             int temp = nums[j];
+        //             nums[j] = nums[j+1];
+        //             nums[j+1] = temp;
+        //         }
+        //     }
+        // }
+        
+        int i =0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j] !=0){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
             }
         }
     }
